9ec790a0c6ae1c0e6212615ca0397aba8822d1ab
Kelly Luck <toymaker@usa.net>
2025-09-17 23:13:03 -0500

    Added actual rename/copy functionality

--- OCRenamer/Form1.cs
+++ OCRenamer/Form1.cs
@@ -14,11 +14,13 @@ public partial class Form1 : Form
     private void button1_Click(object sender, EventArgs e)
     {
         Go("F:\\Documents\\Source\\Repos\\kellyluck\\OCRenamer\\OCRenamer\\MatineeBnR.json",
-           "I:\\Radio Programs\\Bob & Ray\\Matinee With Bob Ray\\RenameUs\\*.mp3");
+           "I:\\Radio Programs\\Bob & Ray\\Matinee With Bob Ray\\*.mp3",
+           "I:\\Radio Programs\\Bob & Ray\\Matinee With Bob Ray\\RenameUs\\",
+           true);
 
     }
 
-    public void Go(string ruleSetPath, string filesPath)
+    public void Go(string ruleSetPath, string filesPath, string destinationPath,bool overwriteExisting)
     {
         RuleSet ruleSet = RuleLoader.LoadRulesFromJson(ruleSetPath);
         List<FilenameInfo> files = FileFinder.GetFilesFromPattern(filesPath);
@@ -36,6 +38,27 @@ public partial class Form1 : Form
             }
 
             System.Diagnostics.Debug.WriteLine($"'{file.FileNameWithoutExtension}' is now '{baseName}'.");
+            string newPath = Path.Combine(destinationPath, $"{baseName}.{file.Extension}");
+
+            if (file.DirectoryPath.Equals(destinationPath))
+            {
+                // direct rename
+                if ((!File.Exists(newPath)) || (File.Exists(newPath) && overwriteExisting))
+                    File.Copy(file.FullPath, newPath, overwriteExisting);
+            }
+            else
+            {
+                // copy to different directory
+                if (!Directory.Exists(destinationPath)) 
+                    Directory.CreateDirectory(destinationPath);
+                
+                if ( (!File.Exists(newPath)) || (File.Exists(newPath) && overwriteExisting) ) 
+                    File.Copy(file.FullPath,newPath,overwriteExisting);
+            }
+
+            /* TODO: 
+             * get some sort of UX together
+             */
         }
     }
 }
